ebe534be37b0926b850499afae10e7341957bc60
Tomas Radic <tomasradic00@gmail.com>
2023-07-26 20:00:05 +0200

    setUp file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      vsm: "12px",
+      sm: "14px",
+      base: "16px",
+      xl: "24px",
+      xxl: "30px",
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
